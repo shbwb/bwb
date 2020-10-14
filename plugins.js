dd07fabc0566eb02ca3e5c694e2da04961fbe283
Chen <xiaoqi77614@icloud.com>
2020-10-14 20:24:02 +0800

    Create plugins.js

---
+++ plugins.js
@@ -0,0 +1,5 @@
+function snackbar() {
+    var x = document.getElementById("snackbar")
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
