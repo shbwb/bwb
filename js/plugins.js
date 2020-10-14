613d81d477f00dd0d44e8bbf616b9349b34e5ed2
Chen <xiaoqi77614@icloud.com>
2020-10-14 22:57:01 +0800

    Update plugins.js

--- js/plugins.js
+++ js/plugins.js
@@ -3,3 +3,19 @@ function snackbar() {
     x.className = "show";
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
+function dropdown() {
+    document.getElementById("dropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
