c416a1faa80adf1e1dec315f4c92885dec7ba763
Chen <xiaoqi77614@icloud.com>
2020-10-15 19:30:29 +0800

    Update plugins.js

--- js/plugins.js
+++ js/plugins.js
@@ -1,11 +1,17 @@
+/* 
+plugins.js: copyright (c) crq(crqblog)(crq.js.org)2020
+The script was written by runoob.com and crqblog
+If you want to copy the code,please tell us.
+Welcome to use
+*/
 function snackbar() {
     var x = document.getElementById("snackbar")
     x.className = "show";
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
 function opennav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("sidenav").style.width = "250px";
 }
 function closenav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("sidenav").style.width = "0";
 }
