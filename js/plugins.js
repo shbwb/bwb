35150be8693038dfb0416b20ba18a3e6d23265fb
Chen <xiaoqi77614@icloud.com>
2020-11-07 17:29:21 +0800

    Update plugins.js

--- js/plugins.js
+++ js/plugins.js
@@ -10,13 +10,6 @@ function snackbar() {
     x.className = "show";
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
-// sidenav
-function opennav() {
-    document.getElementById("sidenav").style.width = "250px";
-}
-function closenav() {
-    document.getElementById("sidenav").style.width = "0";
-}
 // enter
 var input = document.getElementById("enter");
 input.addEventListener("keyup", function(event) {
@@ -25,11 +18,6 @@ input.addEventListener("keyup", function(event) {
         document.getElementById("enterbutton").click();
     }
 });
-// information
-function info() {
-    var info = document.getElementById("info");
-    info.classList.toggle("show");
-}
 // history
 function goback(){
 	window.history.back()
