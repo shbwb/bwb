4bf67a77035008dce270b3e247157af0fffd2b31
Chen <xiaoqi77614@icloud.com>
2020-11-22 06:19:32 +0800

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,50 +1,50 @@
-        var time= new Date();//获取当前日期和对象
-                var y=time.getFullYear();//获取年份
-                 var M=time.getMonth()+1;//获取月份
+var time= new Date();
+        var y=time.getFullYear();
+        var M=time.getMonth()+1;
                 if(M<10){
-                   M="0"+M;
+                 M="0"+M;
                  }
-                 var d=time.getDate();//获取日期
+                 var d=time.getDate();
                  if(d<10){
-                     d="0"+d;
+                 d="0"+d;
                  }
-                 var h=time.getHours();//获取小时
+                 var h=time.getHours();
                  if(h<10){
                      h="0"+h;
                  }
-                 var m=time.getMinutes();//获取分钟
+                 var m=time.getMinutes();
                  if(m<10){
                     m="0"+m;
                  }
-                 var s=time.getSeconds();//获取秒数
+                 var s=time.getSeconds();
                  if(s<10){
                      s="0"+s;
                  }
-                 var day=time.getDay();//获取星期几
+                 var day=time.getDay();
                  switch(day){
                      case 0:
-                       day="天";
+                       day="Sun";
                        break;
                        case 1:
-                       day="一";
+                       day="Mon";
                        break;
                        case 2:
-                       day="二";
+                       day="Tues";
                        break;
                         case 3:
-                       day="三";
+                       day="Wed";
                        break; 
                        case 4:
-                       day="四";
+                       day="Thur";
                        break;
                         case 5:
-                       day="五";
+                       day="Fri";
                        break; 
                        case 6:
-                       day="六";
+                       day="Sat";
                        break;
                     }
-                    var str=y+"年"+M+"月"+d+"日"+" "+h+"时"+m+"分"+s+"秒"+" "+"星期"+day;
+                    var str=y+"/"+M+"/"+d+"/"+" "+h+"h "+m+"min "+s+"s "+" "+"week:"+day;
                      document.write(str);
 
 
