e4e60152c198ac37bc2df113a63281274453fa82
Chen <xiaoqi77614@icloud.com>
2021-01-01 18:38:07 +0800

    Update build.js

--- start/build.js
+++ start/build.js
@@ -1,8 +1,16 @@
+/* 
+plugins.js: copyright (c) crq(shbwb)(crq.js.org,bwb.js.org)2020
+The script was written by qbsz(shbwb).
+If you want to copy the code,please tell us.
+Welcome to use
+*/
+// snackbar
 function snackbar() {
     var x = document.getElementById("snackbar")
     x.className = "show";
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
+// enter
 var input = document.getElementById("enter");
 input.addEventListener("keyup", function(event) {
     event.preventDefault();
@@ -10,21 +18,22 @@ input.addEventListener("keyup", function(event) {
         document.getElementById("enterbutton").click();
     }
 });
+// timer (if a user enter the code <button onclick="timer(time=3000,text='hello')">Front button</button> the box will show after 3 seconds.
 function timer() {
     setTimeout(function(){ alert(text); }, time);
 }
-var vartime=setInterval(function(){clock()},1);
-function clock(){
-	var d=new Date();
-	var t=d.toLocaleTimeString();
-	document.getElementById("clock").innerHTML=t;
-}
+/* ----------------------------------------------------------------*/
+/* browser */
+/* ----------------------------------------------------------------*/
+
+// history
 function goback(){
 	window.history.back()
 }
 function goforward(){
 	window.history.forward()
 }
+// reload
 function reload(){
   location.reload()
 }
