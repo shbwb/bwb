24c65f550f38f9585258e1fe16544330d2350b75
Chen <xiaoqi77614@icloud.com>
2021-01-12 19:43:09 +0800

    Update plugins.js

--- js/plugins.js
+++ js/plugins.js
@@ -1,41 +1,7 @@
-/* 
-plugins.js: copyright (c) crq(shbwb)(crq.js.org,bwb.js.org)2020
-The script was written by qbsz(shbwb).
-If you want to copy the code,please tell us.
-Welcome to use
-*/
-// snackbar
 function snackbar() {
     var x = document.getElementById("snackbar")
     x.className = "show";
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-}
-// enter
-var input = document.getElementById("enter");
-input.addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementById("enterbutton").click();
-    }
-});
-// timer (if a user enter the code <button onclick="timer(time=3000,text='hello')">Front button</button> the box will show after 3 seconds.
-function timer() {
-    setTimeout(function(){ alert(text); }, time);
-}
-/* ----------------------------------------------------------------*/
-/* browser */
-/* ----------------------------------------------------------------*/
-
-// history
-function goback(){
-	window.history.back()
-}
-function goforward(){
-	window.history.forward()
-}
-// reload
-function reload(){
-  location.reload()
 }
 $(document).ready(function(){
   $(".dropdown").click(function(){
