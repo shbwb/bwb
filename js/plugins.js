2375cb916c428ef31b33ada806aad1f530fb4d80
Chen <xiaoqi77614@icloud.com>
2021-01-14 18:34:32 +0800

    Update plugins.js

--- js/plugins.js
+++ js/plugins.js
@@ -3,8 +3,3 @@ function snackbar() {
     x.className = "show";
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
-$(document).ready(function(){
-  $(".dropdown").click(function(){
-    $("#dropdown-content").slideToggle("slow");
-  });
-});
