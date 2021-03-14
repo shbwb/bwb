3085dac8262eff5316ad3acabe1ad397eec42b9a
Chen <xiaoqi77614@icloud.com>
2021-03-14 19:03:49 +0800

    Update plugins.js

--- js/plugins.js
+++ js/plugins.js
@@ -1,3 +1,9 @@
+/*!
+ * BWB (http://bwb.js.org)
+ * Copyright (c) 2021 bwb.js.org
+ * Licensed under MIT (https://github.com/shbwb/bwb/blob/main/LICENSE)
+ */
+/*! plugins.js | MIT License */
 function snackbar() {
     var x = document.getElementById("snackbar")
     x.className = "show";
