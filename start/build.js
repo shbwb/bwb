2aa4e3bad0e6fca99ec2c21155d7cd5f05530197
Chen <xiaoqi77614@icloud.com>
2021-03-14 19:10:04 +0800

    Update build.js

--- start/build.js
+++ start/build.js
@@ -1,8 +1,11 @@
+/*!
+ * BWB (http://bwb.js.org)
+ * Copyright (c) 2021 bwb.js.org
+ * Licensed under MIT (https://github.com/shbwb/bwb/blob/main/LICENSE)
+ */
+/*! build.js | MIT License */
 function snackbar() {
     var x = document.getElementById("snackbar")
     x.className = "show";
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-}
-function timer() {
-	setTimeout(function() {alert(text)},time)
 }
